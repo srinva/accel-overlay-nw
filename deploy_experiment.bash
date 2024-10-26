f9daa6c6fdf0dbd872834c859ffb546c27822436
Srinath Vasudevan <sv072002@gmail.com>
2024-10-26 12:30:24 -0400

    typo

--- deploy_experiment.bash
+++ deploy_experiment.bash
@@ -1,7 +1,7 @@
 set -e
 replicas=$1
-helm upgrade --install accel-overlay-nw-server ./accel-overlay-nw-server --set replicas=$replicas
-helm upgrade --install accel-overlay-nw-client ./accel-overlay-nw-client --set replicas=$replicas
+helm upgrade --install accel-overlay-nw-server ./server --set replicas=$replicas
+helm upgrade --install accel-overlay-nw-client ./client --set replicas=$replicas
 sleep 31
 for i in $(seq 1 $replicas); do
   kubectl logs iperf-server-$i | tail -n 3
