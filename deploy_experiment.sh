8e367d82e13508662c0cc7852b5cc66ca15511fc
Srinath Vasudevan <sv072002@gmail.com>
2024-10-27 11:18:43 -0400

    update script to wait

--- deploy_experiment.sh
+++ deploy_experiment.sh
@@ -3,9 +3,9 @@
 set -e
 replicas=$1
 helm upgrade --install accel-overlay-nw-server ./server --set replicas=$replicas
-sleep 5
+kubectl wait --for=condition=Ready pod -l type=server --timeout=300s
 helm upgrade --install accel-overlay-nw-client ./client --set replicas=$replicas
-sleep 31
+kubectl wait --for=condition=complete pod -l type=client --timeout=300s
 for i in $(seq 1 $replicas); do
   echo "Server $i"
   kubectl logs iperf-server-$i | tail -n 3
