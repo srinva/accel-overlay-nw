dde90ca8534a388a40b1abea19326a132bf07349
Srinath Vasudevan <sv072002@gmail.com>
2024-10-27 11:02:30 -0400

    sleep to wait to get ready

--- deploy_experiment.bash
+++ deploy_experiment.bash
@@ -1,8 +1,10 @@
 set -e
 replicas=$1
 helm upgrade --install accel-overlay-nw-server ./server --set replicas=$replicas
+sleep 5
 helm upgrade --install accel-overlay-nw-client ./client --set replicas=$replicas
 sleep 31
 for i in $(seq 1 $replicas); do
+  echo "Server $i"
   kubectl logs iperf-server-$i | tail -n 3
 done
