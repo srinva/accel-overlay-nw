9f14a24e2f26ebf5baa4d6823ebb42c96223ad8f
Srinath Vasudevan <sv072002@gmail.com>
2024-10-27 11:30:44 -0400

    update sleep

--- deploy_experiment.sh
+++ deploy_experiment.sh
@@ -5,7 +5,8 @@ replicas=$1
 helm upgrade --install accel-overlay-nw-server ./server --set replicas=$replicas
 kubectl wait --for=condition=Ready pod -l type=server --timeout=300s
 helm upgrade --install accel-overlay-nw-client ./client --set replicas=$replicas
-kubectl wait --for=condition=complete pod -l type=client --timeout=300s
+# kubectl wait --for=condition=complete pod -l type=client --timeout=300s
+sleep 40
 for i in $(seq 1 $replicas); do
   echo "Server $i"
   kubectl logs iperf-server-$i | tail -n 3
