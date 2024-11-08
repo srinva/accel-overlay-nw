fa65bce3754202f61adc73fa4c3c21e6bb1b8ebc
Srinath Vasudevan <sv072002@gmail.com>
2024-11-08 13:16:59 -0500

    ssh command in background

--- deploy_experiment.sh
+++ deploy_experiment.sh
@@ -10,14 +10,14 @@ kubectl wait --for=condition=Ready pod -l type=server --timeout=300s
 helm upgrade --install accel-overlay-nw-client ./client --set replicas=$replicas
 # kubectl wait --for=condition=complete pod -l type=client --timeout=300s
 sleep 5
-ssh server1 'mpstat -P ALL 1 > /tmp/mpstat_server1.log &'   # Start mpstat in background
-ssh server2 'mpstat -P ALL 1 > /tmp/mpstat_server2.log &'   # Start mpstat in background
+ssh $server1 'mpstat -P ALL 1 > /tmp/mpstat_server1.log &'&   # Start mpstat in background
+ssh $server2 'mpstat -P ALL 1 > /tmp/mpstat_server2.log &'&   # Start mpstat in background
 sleep 20
-ssh server1 'pkill -SIGINT mpstat'
-ssh server2 'pkill -SIGINT mpstat'
+ssh $server1 'pkill -SIGINT mpstat'
+ssh $server2 'pkill -SIGINT mpstat'
 # Save the last 6 lines of mpstat output to log files
-ssh server1 'tail -n 6 /tmp/mpstat_server1.log' > mpstat_client.log
-ssh server2 'tail -n 6 /tmp/mpstat_server2.log' > mpstat_server.log
+ssh $server1 'tail -n 6 /tmp/mpstat_server1.log' > mpstat_client.log
+ssh $server2 'tail -n 6 /tmp/mpstat_server2.log' > mpstat_server.log
 sleep 15
 for i in $(seq 1 $replicas); do
   echo "Server $i:"
