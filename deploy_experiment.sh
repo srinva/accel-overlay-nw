137012d39f350b5e2bc40d256b42ee1d342f4212
Srinath Vasudevan <svasude5@ncsu.edu>
2024-11-11 11:45:38 -0500

    comment out ssh for now

--- deploy_experiment.sh
+++ deploy_experiment.sh
@@ -10,20 +10,20 @@ kubectl wait --for=condition=Ready pod -l type=server --timeout=300s
 helm upgrade --install accel-overlay-nw-client ./client --set replicas=$replicas
 # kubectl wait --for=condition=complete pod -l type=client --timeout=300s
 sleep 5
-ssh $server1 'mpstat -P ALL 1 > /tmp/mpstat_server1.log &'&   # Start mpstat in background
-ssh $server2 'mpstat -P ALL 1 > /tmp/mpstat_server2.log &'&   # Start mpstat in background
+# ssh $server1 'mpstat -P ALL 1 > /tmp/mpstat_server1.log &'&   # Start mpstat in background
+# ssh $server2 'mpstat -P ALL 1 > /tmp/mpstat_server2.log &'&   # Start mpstat in background
 sleep 20
-ssh $server1 'pkill -SIGINT mpstat'
-ssh $server2 'pkill -SIGINT mpstat'
+# ssh $server1 'pkill -SIGINT mpstat'
+# ssh $server2 'pkill -SIGINT mpstat'
 # Save the last 6 lines of mpstat output to log files
-ssh $server1 'tail -n 6 /tmp/mpstat_server1.log' > mpstat_client.log
-ssh $server2 'tail -n 6 /tmp/mpstat_server2.log' > mpstat_server.log
+# ssh $server1 'tail -n 6 /tmp/mpstat_server1.log' > mpstat_client.log
+# ssh $server2 'tail -n 6 /tmp/mpstat_server2.log' > mpstat_server.log
 sleep 15
 for i in $(seq 1 $replicas); do
-  echo "Server $i:"
+  # echo "Server $i:"
   kubectl logs iperf-server-$i | tail -n 3
-  echo "Client $i:"
-  kubectl logs iperf-client-$i | tail -n 5 | head -n 2
+  # echo "Client $i:"
+  # kubectl logs iperf-client-$i | tail -n 5 | head -n 2
 done
 # cleanup
 helm uninstall accel-overlay-nw-client
