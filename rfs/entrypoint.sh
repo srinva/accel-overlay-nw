9ec02ec42795a7e7ef4247760826a2bac50196c7
Srinath Vasudevan <sv072002@gmail.com>
2024-11-22 00:21:43 -0500

    change to rw

--- rfs/entrypoint.sh
+++ rfs/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
+mount -o remount,rw /sys
 /usr/local/bin/enable_rfs.sh
 exec iperf3 "$@"
--- rps/entrypoint.sh
+++ rps/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
+mount -o remount,rw /sys
 /usr/local/bin/enable_rps.sh
 exec iperf3 "$@"
